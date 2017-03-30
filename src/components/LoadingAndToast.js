8aeec224e7f49aca5b66b4589edacc44a3f67676
Peter Xu <xzm920@163.com>
2017-03-30 15:00:32 +0800

    完善loadingAndToast样式

--- src/components/LoadingAndToast.js
+++ src/components/LoadingAndToast.js
@@ -3,19 +3,58 @@ import { connect } from 'react-redux'
 
 function LoadingAndToast(props) {
 
+  var wrapStyle = {
+    
+  }
+  
   var style = {
-    padding: '20px',
-    display: props.isFetching || props.isError ? 'block' : 'none',
     position: 'absolute',
     left: '50%',
-    bottom: '50%',
-    backgroundColor: 'lightGray',
-    zIndex: 99
+    top: '200px',
+    zIndex: 99,
+    marginLeft: '-50px',
+    display: props.isFetching || props.isError ? 'flex' : 'none',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100px',
+    height: '100px',
+    borderRadius: '5px',
+    backgroundColor: 'rgba(17,17,17,.7)',
+  }
+
+  var loadingIconStyle = {
+    marginBottom: '4px',
+  }
+
+  var textStyle = {
+    fontSize: '16px',
+    textAlign: 'center',   
+    color: '#fff',
+    maxHeight: '3em',
+    lineHeight: '1.5',
+    overflow: 'hidden',
+  }
+
+  if (props.isError) {
+    style.width = '200px'
+    style.marginLeft = '-100px'
   }
 
   var message = props.isError ? props.errorMsg : '请稍等...'
 
-  return <p style={style}>{message}</p>
+  if (props.isFetching) {
+    var loadingIcon = <img src="images/loading2.svg" style={loadingIconStyle} alt=""/>
+  }
+
+  return (
+    <div style={wrapStyle}>
+      <div style={style}>
+        {loadingIcon}
+        <p style={textStyle}>{message}</p>
+      </div>
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
