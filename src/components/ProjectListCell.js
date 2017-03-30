e8243e91c7bd08495fb9a538ee9688b9df69a530
Oliver <olimovier@gmail.com>
2017-03-30 11:34:38 +0800

    样式：调整了首页项目列表的样式

--- src/components/ProjectListCell.js
+++ src/components/ProjectListCell.js
@@ -1,38 +1,77 @@
 import React from 'react'
 
+var containerStyle = {
+  padding: '7px',
+  marginBottom: '2px',
+  backgroundColor: 'white'
+}
+
 var contentWapperStyle = {
+  padding: '6px 20px 6px 10px',
   float: 'left',
   width: '66%'
 }
 
 var imgWrapper = {
-  width: '100px',
-  height: '80px'
+  display: 'block',
+  width: '34%',
+  height: '104px'
+}
+
+var titleStyle = {
+  color: '#666666',
+  fontSize: '15px'
 }
 
 var countryAndIndustrys = {
-  fontSize: '12px',
-  color: 'gray'
+  marginTop: '10px',
+  fontSize: '13px',
+  color: '#999999'
+}
+
+var countryStyle = {
+  display: 'block',
+  float: 'left',
+  width: '60px'
+}
+
+var amountContainerStyle = {
+  marginTop: '10px',
+  fontSize: '13px',
+  color: '#666666'
 }
 
 var amount = {
-  color: 'orange'
+  color: '#FF8F40'
 }
 
 function ProjectListCell(props) {
   return (
-    <div>
+    <div style={containerStyle}>
+
       <div style={contentWapperStyle}>
-        <p>{props.title}</p>
-        <p style={countryAndIndustrys}><span>{props.country}</span><span>{props.industrys}</span></p>
-        <p style={countryAndIndustrys}>交易规模：<span style={amount}>${props.amount}</span></p>
+        <p style={titleStyle}>{sliceStringIfTooLong(props.title, 20)}</p>
+
+        <p style={countryAndIndustrys}>
+          <span style={countryStyle}>{sliceStringIfTooLong(props.country, 3)}</span>
+          <span>{sliceStringIfTooLong(props.industrys, 6)}</span>
+        </p>
+
+        <p style={amountContainerStyle}>交易规模：<span style={amount}>${new Intl.NumberFormat().format(props.amount)}</span></p>
       </div>
-      <div >
-        <img style={imgWrapper} src={props.imgUrl} alt={props.title} />
-      </div>
-      <hr />
+
+      <img style={imgWrapper} src={props.imgUrl} alt={props.title} />
+
     </div>
   )
+}
+
+function sliceStringIfTooLong(str, maxLength) {
+  if (str.length <= maxLength) {
+    return str
+  } else {
+    return str.substr(0, maxLength) + '...'
+  }
 }
 
 export default ProjectListCell
