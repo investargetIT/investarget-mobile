910a9c7bf10b1e6fce4cf6f7f1b4d2aaa9353353
Oliver <olimovier@gmail.com>
2017-04-18 23:09:35 +0800

    Ready to upload business card

--- src/containers/ModifyBusinessCard.js
+++ src/containers/ModifyBusinessCard.js
@@ -9,10 +9,46 @@ const containerStyle = {
   minHeight: window.innerHeight - 48 + 'px'
 }
 
+const cardContainerStyle = {
+  padding: '0px 16px',
+  backgroundColor: 'white',
+  minHeight: window.innerHeight - 43*4 - 48
+}
+
+const cardStyle = {
+  width: '100%'
+}
+
+const cardTitleStyle = {
+  lineHeight: '36px'
+}
+
+const cardTipStyle = {
+  color: 'darkRed'
+}
+
+const cardImageContainerStyle = {
+  position: 'relative'
+}
+
+const inputContainerStyle = {
+  position: 'absolute',
+  width: '100%',
+  top: 0,
+  left: 0
+}
+
+const inputStyle = {
+  width: '100%',
+  height: '200px',
+  opacity: 0
+}
+
 class ModifyBusinessCard extends Component {
 
   constructor(props) {
     super(props)
+    
     this.handleItemClicked = this.handleItemClicked.bind(this)
   }
 
@@ -27,12 +63,27 @@ class ModifyBusinessCard extends Component {
         <NavigationBar title="修改名片" />
 
         <div style={containerStyle}>
+
           <div onClick={this.handleItemClicked}>
             <LeftLabelRightContent label="姓名" content={this.props.userInfo.name} />
             <LeftLabelRightContent label="公司" content={this.props.userInfo.company} />
             <LeftLabelRightContent label="职位" content={this.props.userInfo.title.titleName} />
             <LeftLabelRightContent label="邮箱" content={this.props.userInfo.emailAddress} />
           </div>
+
+          <div style={cardContainerStyle}>
+
+            <p style={cardTitleStyle}>我的名片:<span style={cardTipStyle}>(名片请横向放置)</span></p>
+
+            <div style={cardImageContainerStyle}>
+              <img style={cardStyle} src="/images/userCenter/emptyCardImage@2x.png" />
+              <div style={inputContainerStyle}>
+                <input style={inputStyle} id="file" type="file" accept="image/*" onChange={this.handleInputChange} />
+              </div>
+            </div>
+
+          </div>
+
         </div>
 
       </div>
