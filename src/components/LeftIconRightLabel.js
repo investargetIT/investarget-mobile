7fd76d1b3ae92fc70c42955d7996be9f43b54288
Oliver <olimovier@gmail.com>
2017-04-22 21:25:11 +0800

    Make user center beautiful

--- src/components/LeftIconRightLabel.js
+++ src/components/LeftIconRightLabel.js
@@ -1,23 +1,83 @@
 import React from 'react'
 
-var itemContainerStyle = {
-  display: 'flex',
-  padding: '6px 30px',
-  alignItems: 'center',
-}
+const height = 44
 
 var iconStyle = {
   width: '20px',
-  height: 'auto',
-  marginRight: '14px'
+  height: '20px',
+  margin: (height - 20) / 2 + 'px 0px'
+}
+
+const leftIconContainerStyle = {
+  float: 'left',
+  width: '42px',
+  height: height + 'px'
+}
+
+const rightIconContainerStyle = {
+  float: 'right',
+  width: '48px',
+  height: height + 'px',
+  textAlign: 'center'
+}
+
+const labelContainerStyle = {
+  lineHeight: height + 'px',
+  color: '#333',
+  fontSize: '16px'
+}
+
+const rightIconStyle = {
+  width: '24px',
+  height: '24px',
+  margin: (height - 24) / 2 + 'px 0px'
+}
+
+const container = {
+  position: 'relative',
+  marginLeft: '22px',
+}
+
+var itemContainerStyle = {
+  zIndex: 2,
+  position: 'relative'
 }
 
 function LeftIconRightLabel(props) {
+
+  const border = {
+    WebkitTransform: 'scale(0.5)',
+    transform: 'scale(0.5)',
+    position: 'absolute',
+    borderBottom: '1px solid lightGray',
+    top: '-50%',
+    right: '-50%',
+    bottom: '-50%',
+    left: '-50%',
+  }
+
+  if (props.hideBorder) {
+    delete border.borderBottom
+  }
+
   return (
-    <li style={itemContainerStyle} onClick={props.onClick}>
-      <img alt="" style={iconStyle} src={props.icon} />
-      <span>{props.label}</span>
-    </li>
+    <div style={container}>
+
+      <div style={itemContainerStyle} onClick={props.onClick}>
+        <div style={leftIconContainerStyle}>
+          <img alt="" style={iconStyle} src={props.icon} />
+        </div>
+
+        <div style={rightIconContainerStyle}>
+          <img alt="" style={rightIconStyle} src="/images/userCenter/ic_chevron_right_black_24px.svg" />
+        </div>
+
+        <div style={labelContainerStyle}><span>{props.label}</span></div>
+      </div>
+
+      <div style={border}></div>
+
+    </div>
   )
 }
 
--- src/containers/User.js
+++ src/containers/User.js
@@ -6,12 +6,41 @@ import { Redirect, Link } from 'react-router-dom'
 import { logout, handleError, requestContents, hideLoading, modifyUserInfo } from '../actions'
 import api from '../api'
 
+const groupStyle = {
+  position: 'relative'
+}
+const border = {
+  WebkitTransform: 'scale(0.5)',
+  transform: 'scale(0.5)',
+  position: 'absolute',
+  borderTop: '1px solid lightGray',
+  borderBottom: '1px solid lightGray',
+  top: '-50%',
+  right: '-50%',
+  bottom: '-50%',
+  left: '-50%',
+}
+const container = {
+  position: 'relative',
+  marginBottom: '16px',
+  backgroundColor: 'white'
+}
+function Group(props) {
+  return (
+    <div style={container}>
+      <div style={groupStyle}>{props.children}</div>
+      <div style={border}></div>
+    </div>
+  )
+}
+
 var blurBackgroundStyle = {
   position: 'absolute',
   width: '100%',
   height: '240px',
   zIndex: -1,
   overflow: 'hidden',
+  WebkitTransform: 'translate3d(0,0,0)'
 }
 
 var blurImageStyle = {
@@ -66,6 +95,14 @@ var inputStyle = {
   width: '90px',
   height: '90px',
   opacity: 0
+}
+
+const placeholderStyle = {
+  position: 'fixed',
+  backgroundColor: 'rgb(238, 243, 244)',
+  minHeight: window.innerHeight - 48 + 'px',
+  width: '100%',
+  zIndex: -1
 }
 
 class User extends Component {
@@ -147,65 +184,72 @@ class User extends Component {
     return (
       <div>
 
+        <div style={placeholderStyle}></div>
+
         <div style={blurBackgroundStyle}>
           <div style={blurImageStyle}></div>
         </div>
+        
         <div style={headerContainerStyle}>
-	  <div style={avatarContainerStyle}>
-	    <img alt="" style={avatarStyle} src={this.state.avatar || this.props.userInfo.photoUrl} />
-	    <div style={inputContainerStyle}><input style={inputStyle} id="file" type="file" accept="image/*" onChange={this.handleAvatarChange} /></div>
-	  </div>
+          <div style={avatarContainerStyle}>
+            <img alt="" style={avatarStyle} src={this.state.avatar || this.props.userInfo.photoUrl} />
+            <div style={inputContainerStyle}><input style={inputStyle} id="file" type="file" accept="image/*" onChange={this.handleAvatarChange} /></div>
+          </div>
           <p style={orgNameStyle}>{this.props.userInfo.company}</p>
           <p><span style={nameAndTitleStyle}>{this.props.userInfo.name}</span><span style={nameAndTitleStyle}>{this.props.userInfo.title.titleName}</span></p>
         </div>
 
         <div style={settingContainerStyle}>
           <ul>
-            {
-              this.props.userInfo.userType === 3 ? (
-                <Link to="/my_partener">
-                  <LeftIconRightLabel icon="images/userCenter/ht-usercenter-6@2x.png" label="我的投资人" />
-                </Link>
-              ) : null
-            }
+            <Group>
+              {
+                this.props.userInfo.userType === 3 ? (
+                  <Link to="/my_partener">
+                    <LeftIconRightLabel icon="images/userCenter/ht-usercenter-6@2x.png" label="我的投资人" />
+                  </Link>
+                ) : null
+              }
 
-            {
-              this.props.userInfo.userType === 1 ? (
-                <Link to="/my_partener">
-                  <LeftIconRightLabel icon="images/userCenter/ht-usercenter-6@2x.png" label="我的交易师" />
-                </Link>
-              ) : null
-            }
+              {
+                this.props.userInfo.userType === 1 ? (
+                  <Link to="/my_partener">
+                    <LeftIconRightLabel icon="images/userCenter/ht-usercenter-6@2x.png" label="我的交易师" />
+                  </Link>
+                ) : null
+              }
 
-            <Link to="/notification">
-              <LeftIconRightLabel icon="images/notifyMessage@2x.png" label="通知消息" />
-            </Link>
+              <Link to="/notification">
+                <LeftIconRightLabel icon="images/notifyMessage@2x.png" label="通知消息" hideBorder={true} />
+              </Link>
+            </Group>
+            <Group>
+              <Link to="/my_tag">
+                <LeftIconRightLabel icon="images/userCenter/ht-usercenter-1@2x.png" label="关注的标签" />
+              </Link>
 
-            <Link to="/my_tag">
-              <LeftIconRightLabel icon="images/userCenter/ht-usercenter-1@2x.png" label="关注的标签" />
-            </Link>
+              <Link to="/timeline_management">
+                <LeftIconRightLabel icon="images/userCenter/ht-usercenter-9@2x.png" label="时间轴管理" />
+              </Link>
 
-            <Link to="/timeline_management">
-              <LeftIconRightLabel icon="images/userCenter/ht-usercenter-9@2x.png" label="时间轴管理" />
-            </Link>
+              <Link to="/my_favorite_project">
+                <LeftIconRightLabel icon="images/userCenter/ht-usercenter-3@2x.png" label="收藏的项目" hideBorder={true} />
+              </Link>
+            </Group>
+            <Group>
+              <Link to="/modify_password">
+                <LeftIconRightLabel icon="images/userCenter/ht-usercenter-5@2x.png" label="修改密码" />
+              </Link>
 
-            <Link to="/my_favorite_project">
-              <LeftIconRightLabel icon="images/userCenter/ht-usercenter-3@2x.png" label="收藏的项目" />
-            </Link>
-            
-            <Link to="/modify_password">
-              <LeftIconRightLabel icon="images/userCenter/ht-usercenter-5@2x.png" label="修改密码" />
-            </Link>
+              <Link to="/modify_business_card">
+                <LeftIconRightLabel icon="images/userCenter/name_card@2x.png" label="修改名片" />
+              </Link>
 
-            <Link to="/modify_business_card">
-              <LeftIconRightLabel icon="images/userCenter/name_card@2x.png" label="修改名片" />
-            </Link>
-
-            <Link to="/contact">
-              <LeftIconRightLabel icon="images/userCenter/ht-usercenter-8@2x.png" label="联系我们" />
-            </Link>
-
-            <LeftIconRightLabel icon="images/userCenter/ht-usercenter-9@2x.png" label="退出登录" onClick={this.handleClick} />
+              <Link to="/contact">
+                <LeftIconRightLabel icon="images/userCenter/ht-usercenter-8@2x.png" label="联系我们" hideBorder={true} />
+              </Link>
+            </Group>
+            <Group>
+              <LeftIconRightLabel icon="images/userCenter/ht-usercenter-9@2x.png" label="退出登录" onClick={this.handleClick} hideBorder={true} /></Group>
 
           </ul>
         </div>
