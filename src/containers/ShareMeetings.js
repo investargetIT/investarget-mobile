6a9bb230a4a5e73616edb5186f62311d1e5edad2
Oliver <olimovier@gmail.com>
2018-09-24 22:42:51 +0800

    按照设计实现会议BD分享页面

--- src/containers/ShareMeetings.js
+++ src/containers/ShareMeetings.js
@@ -14,20 +14,36 @@ class ShareMeetings extends React.Component {
     this.key = key;
 
     this.state = {
-      result: '',
+      data: [],
     };
   }
 
   componentDidMount() {
     newApi.getSharedMeeting(this.key)
       .then(result => {
-        this.setState({ result: JSON.stringify(result) });
+        this.setState({ data: result });
       })
       .catch(error => this.props.dispatch(handleError(error)));
   }
 
-  render () {
-    return <div>{this.state.result}</div>;
+  render() {
+    return <div>
+      <div style={{ textAlign: 'center', marginTop: 20 }}><img style={{ width: 160 }} src="/images/logo.png" /></div>
+      <div style={{ textAlign: 'center', marginTop: 10, marginBottom: 20, fontSize: 24, fontWeight: 'bolder' }}>会议安排</div>
+      <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
+      <div style={{ padding: 6 }}>
+        {this.state.data.map(m => <div key={m.id} style={{ marginTop: 10, border: '1px solid lightgray', borderRadius: 6 }}>
+          <div style={{ padding: 10 }}>
+            <div style={{ fontSize: 15, fontWeight: 'bold', color: '#000' }}>{`${m.username}（${m.org && m.org.orgname}）`}</div>
+            <div style={{ marginTop: 6, color: 'gray' }}>{m.meet_date.replace('T', ' ').substring(0, 16)}</div>
+          </div>
+          <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
+          <div style={{ padding: 10, color: 'rgb(176, 176, 176)', fontSize: 13 }}>机构介绍：{m.org && m.org.description ? m.org.description : '暂无'}</div>
+          <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
+          <div style={{ padding: 10, color: 'rgb(176, 176, 176)', fontSize: 13 }}>附件下载：{m.attachment ? <a style={{ color: '#428BCA' }} href={m.attachmenturl}>{m.attachment}</a> : '暂无'}</div>
+        </div>)}
+      </div>
+    </div>;
   }
 }
 
