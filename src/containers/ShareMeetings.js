23ce50fea2a227c427bf2270563b9a88c7a63a4d
Oliver <olimovier@gmail.com>
2018-09-25 13:47:09 +0800

    修改了会议BD分享的一些问题

--- src/containers/ShareMeetings.js
+++ src/containers/ShareMeetings.js
@@ -32,16 +32,22 @@ class ShareMeetings extends React.Component {
       <div style={{ textAlign: 'center', marginTop: 10, marginBottom: 20, fontSize: 24, fontWeight: 'bolder' }}>会议安排</div>
       <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
       <div style={{ padding: 6 }}>
-        {this.state.data.map(m => <div key={m.id} style={{ marginTop: 10, border: '1px solid lightgray', borderRadius: 6 }}>
+        {this.state.data.map(m => m.username ? <div key={m.id} style={{ marginTop: 10, border: '1px solid lightgray', borderRadius: 6 }}>
           <div style={{ padding: 10 }}>
-            <div style={{ fontSize: 15, fontWeight: 'bold', color: '#000' }}>{`${m.username}（${m.org && m.org.orgname}）`}</div>
+            <div style={{ fontSize: 15, fontWeight: 'bold', color: '#000' }}>{m.username + (m.org ? `（${m.org && m.org.orgname}）` : '')}</div>
             <div style={{ marginTop: 6, color: 'gray' }}>{m.meet_date.replace('T', ' ').substring(0, 16)}</div>
           </div>
           <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
           <div style={{ padding: 10, color: 'rgb(176, 176, 176)', fontSize: 13 }}>机构介绍：{m.org && m.org.description ? m.org.description : '暂无'}</div>
-          <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
-          <div style={{ padding: 10, color: 'rgb(176, 176, 176)', fontSize: 13 }}>附件下载：{m.attachment ? <a style={{ color: '#428BCA' }} href={m.attachmenturl}>{m.attachment}</a> : '暂无'}</div>
-        </div>)}
+          {m.isShow ?
+            <div>
+              <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
+              <div style={{ padding: 10, color: 'rgb(176, 176, 176)', fontSize: 13 }}>会议纪要：{m.comments || '暂无'}</div>
+              <div style={{ height: 1, backgroundColor: '#e9e9e9' }} />
+              <div style={{ padding: 10, color: 'rgb(176, 176, 176)', fontSize: 13 }}>附件下载：{m.attachment ? <a style={{ color: '#428BCA' }} href={m.attachmenturl}>{m.attachment}</a> : '暂无'}</div>
+            </div>
+            : null}
+        </div> : null)}
       </div>
     </div>;
   }
