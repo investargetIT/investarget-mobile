5e7cf672d28b54628169e1e333513c758421be04
InvesTarget <system@investarget.com>
2023-02-28 23:52:57 -0800

    Keep working on styles

--- src/components/AdvancedLeftIconRightLabel.js
+++ src/components/AdvancedLeftIconRightLabel.js
@@ -29,7 +29,7 @@ var itemContainerStyle = {
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: '8px 0',
+  padding: '10px 0',
 }
 
 const border = {
@@ -80,11 +80,11 @@ class LeftIconRightLabel extends React.Component {
         <div style={itemContainerStyle} >
           <div style={{ flex: 1, display: 'flex', alignItems: 'center' }}>
             <img alt="" style={{ width: 20 }} src="/images/userCenter/chat_bubble_FILL0_wght400_GRAD0_opsz48.svg" />
-            <div style={{ margin: '0 12px', fontSize: 14, color: '#ececf1' }}>
+            <div style={{ margin: '0 12px', fontSize: 14, color: '#ececf1', flex: 1 }}>
               {!this.state.isEditMode ?
                 <span onClick={() => this.props.onClick(this.props.topicID, this.props.label)}>{this.props.label}</span>
                 :
-                <input value={this.state.inputValue} style={{ color: 'gray' }} onChange={this.handleValueChange} />
+                <input value={this.state.inputValue} style={{ border: 0, fontSize: 14, color: 'inherit', width: '100%', backgroundColor: 'inherit' }} onChange={this.handleValueChange} />
               }
             </div>
           </div>
